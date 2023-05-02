b7f48897639ed9a6410e2175485b68860f6de1c2
Tawonga netcode <netcodemw@gmail.com>
2023-05-02 22:48:32 +0200

    finished project

--- app/src/main/java/com/example/nthandizi_police_service_app_ver1/User_Dashboard_Activity.java
+++ app/src/main/java/com/example/nthandizi_police_service_app_ver1/User_Dashboard_Activity.java
@@ -1,6 +1,5 @@
 package com.example.nthandizi_police_service_app_ver1;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -9,11 +8,6 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
-import android.widget.Toast;
 
 import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.navigation.NavigationView;
@@ -31,45 +25,59 @@ public class User_Dashboard_Activity extends AppCompatActivity {
         NavigationView navigationView = findViewById(R.id.navigation_view);
 
         //when navigation menu icon clicked should open
-        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                drawerLayout.openDrawer(GravityCompat.START);
-            }
-        });
+        toolbar.setNavigationOnClickListener(view -> drawerLayout.openDrawer(GravityCompat.START));
 
         //when navigation items clicked or selected
-        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                int id = item.getItemId();
-                item.setChecked(true);
-                drawerLayout.closeDrawer(GravityCompat.START);
-                switch (id) {
+        navigationView.setNavigationItemSelectedListener(item -> {
+            int id = item.getItemId();
+            item.setChecked(true);
+            drawerLayout.closeDrawer(GravityCompat.START);
 
-                    case R.id.personal_profile_id:
-                        replaceFragment(new Fragment_PersonProfile()); break;
-
-                    case R.id.comm_profile_id:
-                        replaceFragment(new Fragment_CommunityProfile()); break;
-
-                    case R.id.ps_profile_id:
-                        replaceFragment(new Fragment_PoliceProfile()); break;
-
-                    case R.id.viewReport_id:
-                        replaceFragment(new Fragment_ViewReports()); break;
-
-                    case R.id.GeneratedReport_id:
-                        replaceFragment(new Fragment_GeneratedReports()); break;
-
-                    case R.id.NpsInfo_id:
-                        replaceFragment(new FragmentBottom_text_report()); break;
-
-                    default:
-                        return true;
-                }
-                return false;
+            if(id== R.id.personal_profile_id){
+                replaceFragment(new Fragment_PersonProfile());
             }
+            else if(id==R.id.comm_profile_id){
+                replaceFragment(new Fragment_CommunityProfile());
+            }
+            else if(id== R.id.ps_profile_id){
+                replaceFragment(new Fragment_PoliceProfile());
+            }
+            else if(id==R.id.viewReport_id){
+                replaceFragment(new Fragment_ViewReports());
+            }
+            else if(id== R.id.GeneratedReport_id){
+                replaceFragment(new Fragment_GeneratedReports());
+            }
+            else if(id==R.id.NpsInfo_id){
+                replaceFragment(new FragmentBottom_text_report());
+            }
+            else{
+                return true;
+            }
+//            switch (id) {
+//
+//                case R.id.personal_profile_id:
+//                    replaceFragment(new Fragment_PersonProfile()); break;
+//
+//                case R.id.comm_profile_id:
+//                    replaceFragment(new Fragment_CommunityProfile()); break;
+//
+//                case R.id.ps_profile_id:
+//                    replaceFragment(new Fragment_PoliceProfile()); break;
+//
+//                case R.id.viewReport_id:
+//                    replaceFragment(new Fragment_ViewReports()); break;
+//
+//                case R.id.GeneratedReport_id:
+//                    replaceFragment(new Fragment_GeneratedReports()); break;
+//
+//                case R.id.NpsInfo_id:
+//                    replaceFragment(new FragmentBottom_text_report()); break;
+//
+//                default:
+//                    return true;
+//            }
+            return false;
         });
     }
     //to replace framework with fragment
@@ -80,9 +88,4 @@ public class User_Dashboard_Activity extends AppCompatActivity {
         fragmentTransaction.commit();
     }
 
-//    public void OnRadioBtnClicked(View view) {
-//        RadioGroup radioGroup = view.findViewById(R.id.radioGroup_Id);
-//        RadioButton radioButton = view.findViewById(radioGroup.getCheckedRadioButtonId());
-//        Toast.makeText(this, radioButton.getText()+" is selected ", Toast.LENGTH_SHORT).show();
-//    }
 }
