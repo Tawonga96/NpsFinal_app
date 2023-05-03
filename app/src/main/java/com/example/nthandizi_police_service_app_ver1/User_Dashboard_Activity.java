bb88c46ba0b5bd1ac347a81565e59b9a00c22843
Tawonga netcode <netcodemw@gmail.com>
2023-05-03 02:42:23 +0200

    finished project

--- app/src/main/java/com/example/nthandizi_police_service_app_ver1/User_Dashboard_Activity.java
+++ app/src/main/java/com/example/nthandizi_police_service_app_ver1/User_Dashboard_Activity.java
@@ -7,9 +7,11 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
 
 public class User_Dashboard_Activity extends AppCompatActivity {
@@ -27,7 +29,7 @@ public class User_Dashboard_Activity extends AppCompatActivity {
         //when navigation menu icon clicked should open
         toolbar.setNavigationOnClickListener(view -> drawerLayout.openDrawer(GravityCompat.START));
 
-        //when navigation items clicked or selected
+        //when drawer navigation items clicked or selected
         navigationView.setNavigationItemSelectedListener(item -> {
             int id = item.getItemId();
             item.setChecked(true);
@@ -49,35 +51,34 @@ public class User_Dashboard_Activity extends AppCompatActivity {
                 replaceFragment(new Fragment_GeneratedReports());
             }
             else if(id==R.id.NpsInfo_id){
-                replaceFragment(new FragmentBottom_text_report());
+                replaceFragment(new Fragment_Nps_info());
             }
             else{
                 return true;
             }
-//            switch (id) {
-//
-//                case R.id.personal_profile_id:
-//                    replaceFragment(new Fragment_PersonProfile()); break;
-//
-//                case R.id.comm_profile_id:
-//                    replaceFragment(new Fragment_CommunityProfile()); break;
-//
-//                case R.id.ps_profile_id:
-//                    replaceFragment(new Fragment_PoliceProfile()); break;
-//
-//                case R.id.viewReport_id:
-//                    replaceFragment(new Fragment_ViewReports()); break;
-//
-//                case R.id.GeneratedReport_id:
-//                    replaceFragment(new Fragment_GeneratedReports()); break;
-//
-//                case R.id.NpsInfo_id:
-//                    replaceFragment(new FragmentBottom_text_report()); break;
-//
-//                default:
-//                    return true;
-//            }
+
             return false;
+        });
+
+        //when bottom navigation items clicked or selected
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_menu_id);
+        bottomNavigationView.setOnItemReselectedListener(item -> {
+            int bottomNav_id = item.getItemId();
+
+            if(bottomNav_id == R.id.Home_bottom_id){
+                Intent Home_Intent = new Intent(User_Dashboard_Activity.this, User_Dashboard_Activity.class);
+                startActivity(Home_Intent);
+            }
+            else if(bottomNav_id == R.id.message_bottom_id){
+                replaceFragment(new FragmentBottom_text_report());
+            }
+            else if(bottomNav_id == R.id.image_bottom_id){
+                replaceFragment(new FragmentBottom_image_report());
+            }
+            else if(bottomNav_id == R.id.video_bottom_id){
+                replaceFragment(new FragmentBottom_video_report());
+            }
+
         });
     }
     //to replace framework with fragment
